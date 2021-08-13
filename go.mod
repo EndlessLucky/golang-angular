590906bfdcc74a97184c0708d7c08fc91bb73ba6
Olive105 <105.olivesoft@gmail.com>
2021-08-12 22:30:26 -0400

    feat - add auth0

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.16
 replace github.com/olive105/golang-angular/handlers => /handlers
 
 require (
+	github.com/auth0-community/go-auth0 v1.0.0 // indirect
 	github.com/gin-gonic/gin v1.7.3
 	github.com/olive105/golang-angular v0.0.0-20210812142232-4d8cd36e5b8a // indirect
 	github.com/olive105/golang-angular/handlers v0.0.0-00010101000000-000000000000
 	github.com/rs/xid v1.3.0
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
 	"path"
 	"path/filepath"
 
+	"github.com/auth0-community/go-auth0"
 	"github.com/gin-gonic/gin"
+	jose "gopkg.in/square/go-jose.v2"
+
 	"github.com/olive105/golang-angular/handlers"
 )
 
+var (
+	audience string
+	domain   string
+)
+
 func main() {
+	setAuth0Variables()
 	r := gin.Default()
+
 	r.NoRoute(func(c *gin.Context) {
 		dir, file := path.Split(c.Request.RequestURI)
 		exit := filepath.Ext(file)
@@ -20,13 +33,44 @@ func main() {
 		}
 	})
 
-	r.GET("/todo", handlers.GetTodoListHandler)
-	r.POST("/todo", handlers.AddTodoHandler)
-	r.DELETE("/todo/:id", handlers.DeleteTodoHandler)
-	r.PUT("/todo", handlers.CompleteTodoHandler)
+	authorized := r.Group("/")
+	authorized.Use(authRequired())
+	authorized.GET("/todo", handlers.GetTodoListHandler)
+	authorized.POST("/todo", handlers.AddTodoHandler)
+	authorized.DELETE("/todo/:id", handlers.DeleteTodoHandler)
+	authorized.PUT("/todo", handlers.CompleteTodoHandler)
 
 	err := r.Run(":3000")
 	if err != nil {
 		panic(err)
 	}
+}
+
+func setAuth0Variables() {
+	audience = os.Getenv("AUTH0_API_IDENTIFIER")
+	domain = os.Getenv("AUTH0_DOMAIN")
+}
+
+//ValidateRequest will verify that a token recevied from an http request is valid and signed by Auth0
+func authRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var auth0Domain = "https://" + domain + "/"
+		client := auth0.NewJWKClient(auth0.JWKClientOptions{URI: auth0Domain + ".well-known/jwks.json"}, nil)
+		configuration := auth0.NewConfiguration(client, []string{audience}, auth0Domain, jose.RS256)
+		validator := auth0.NewValidator(configuration, nil)
+
+		_, err := validator.ValidateRequest(c.Request)
+
+		if err != nil {
+			log.Println(err)
+			terminateWithError(http.StatusUnauthorized, "token is not valid", c)
+			return
+		}
+		c.Next()
+	}
+}
+
+func terminateWithError(statusCode int, message string, c *gin.Context) {
+	c.JSON(statusCode, gin.H{"error": message})
+	c.Abort()
 }
