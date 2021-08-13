fcb48797f0b1c1c76ee2799e55b7af26d371da0c
Olive105 <105.olivesoft@gmail.com>
2021-08-13 09:59:18 -0400

    feat - add todo service and component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
