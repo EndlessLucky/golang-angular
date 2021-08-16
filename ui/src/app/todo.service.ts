e14d2ae656bfa98bb1ab762d13149c1b63a01564
Olive105 <105.olivesoft@gmail.com>
2021-08-16 19:19:23 -0400

    feat - add CORS allow

--- ui/src/app/todo.service.ts
+++ ui/src/app/todo.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/internal/Observable';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
