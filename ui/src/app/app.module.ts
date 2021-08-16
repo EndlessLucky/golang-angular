ba3321425d96a95a52c07afcc7aa2e6d1247f71c
Olive105 <105.olivesoft@gmail.com>
2021-08-15 21:14:56 -0400

    feat - implement interceptor

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TodoComponent } from './todo/todo.component';
+import { CallbackComponent } from './callback/callback.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TodoComponent
+    TodoComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
