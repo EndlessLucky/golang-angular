67102970ea255d4e82e45471817c2c135942ab4f
Olive105 <105.olivesoft@gmail.com>
2021-08-15 22:31:25 -0400

    feat - add authguard service

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './auth-guard.service';
+import { CallbackComponent } from './callback/callback.component';
+import { TodoComponent } from './todo/todo.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'todo', component: TodoComponent,  canActivate: [AuthGuardService] },
+  { path: 'callback', component: CallbackComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ ui/src/app/auth-guard.service.ts
@@ -0,0 +1,18 @@
+import { CanActivate, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    }
+    this.auth.login();
+    return false;
+  }
+}
